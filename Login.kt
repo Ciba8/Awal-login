a7f65d8240c3d5b97a349117ca5238030c84fd13
Nusaibah Fathul Jannah <138869772+Ciba8@users.noreply.github.com>
2023-07-07 22:42:42 +0700

    Update and rename AwalActivity.kt to Login.kt

--- Login.kt
+++ Login.kt
@@ -1,24 +1,22 @@
-package com.example.talenthub8
+package com.example.talenthubbatch8
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.talenthub8.databinding.ActivityAwalBinding
+import com.example.talenthubbatch8.databinding.ActivityLoginBinding
 
-
-class AwalActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAwalBinding
-
+class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityAwalBinding.inflate(layoutInflater)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         btnGetstartedListener()
     }
 
     private fun btnGetstartedListener() {
-        binding.btn2.setOnClickListener {
+        binding.btn1.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
