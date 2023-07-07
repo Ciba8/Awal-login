ac139bc230f699b3519782edbdf1145131a9f485
Nusaibah Fathul Jannah <138869772+Ciba8@users.noreply.github.com>
2023-07-07 22:43:02 +0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,10 +1,9 @@
-package com.example.talenthub8
+package com.example.talenthubbatch8
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.talenthub8.databinding.ActivityMainBinding
-
+import com.example.talenthubbatch8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,8 +17,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun btnGetstartedListener() {
-        binding.btn1.setOnClickListener {
-            startActivity(Intent(this, AwalActivity::class.java))
+        binding.btn2.setOnClickListener{
+            startActivity(Intent(this, Login::class.java))
         }
     }
 }
